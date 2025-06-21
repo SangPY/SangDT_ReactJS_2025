d14726d25e7c7da975f515d3f18197ba6083e625
SangDoThanh <sangdothanh95@gmail.com>
2025-06-21 22:16:03 +0700

    React js _ Render Hình Ảnh, Dữ Liệu động - Cách Sử Dụng JSX để Hiển Thị Dữ Liệu Linh Hoạt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let today = new Date().toLocaleDateString();
+let time = new Date().toLocaleTimeString();
 
+const Welcome = [
+  "Sang",
+  "Sam",
+  "Sung"
+]
+
+function ranDomTitle() {
+  return Math.floor(Math.random() * Welcome.length);
+}
+
+function Header() {
+  const title = Welcome[ ranDomTitle()];
   return (
     <>
      <h1>DO THANH SANG REACT 2025</h1>
+     <h1>{title}</h1>
+     <p>
+      Hôm nay là: <strong>{today}</strong>. thời gian còn lại {" "}
+      <strong>{time}</strong>
+     </p>
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
     </>
   )
 }
