712ae46db97087307d1003b784b76412e1d7cd66
sangdothanh95 <sangdothanh95@gmail.com>
2025-06-24 13:25:33 +0700

    use State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import MainContent from "./components/MainContent/MainContent.jsx";
 import Header from "./components/Header/Header.jsx";
-import {Sangdata} from "../data.js";
+import {Sangdata, EXAMPLES} from "../data.js";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
-  
-    function handleClick(selectButton) {
-      alert(`${selectButton} được chọn`);
-    }
+  const [selectedTopic, setSelectedTopic] = useState("components");
+  console.log("App được gọi");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
 
   return (
     <>
@@ -26,13 +29,19 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>{handleClick('components')}}>Components</TabButton>
-            <TabButton onSelect={()=>{handleClick('jsx')}}>JSX</TabButton>
-            <TabButton onSelect={()=>{handleClick('props')}}>Props</TabButton>
-            <TabButton onSelect={()=>{handleClick('state')}}>State</TabButton>
+            <TabButton onSelect={()=>{handleSelect('components')}}>Components</TabButton>
+            <TabButton onSelect={()=>{handleSelect('jsx')}}>JSX</TabButton>
+            <TabButton onSelect={()=>{handleSelect('props')}}>Props</TabButton>
+            <TabButton onSelect={()=>{handleSelect('state')}}>State</TabButton>
             {/* <TabButton batky="Components"></TabButton> */}
           </menu>
-          Somee content
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].desc}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </>
